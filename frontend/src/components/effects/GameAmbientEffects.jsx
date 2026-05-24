6bbc91d5fdefef194721623cdee7a8450f0d0697
André Pimentel Santos <andrepimentelsantos01@gmail.com>
2026-05-24 13:15:10 -0300

    fix frame rate.

--- frontend/src/components/effects/GameAmbientEffects.jsx
+++ frontend/src/components/effects/GameAmbientEffects.jsx
@@ -1,18 +1,10 @@
 import { memo } from "react";
-import AmbientGlow from "./AmbientGlow";
 import AnimatedGridOverlay from "./AnimatedGridOverlay";
-import FloatingSqlTokens from "./FloatingSqlTokens";
-import ParticleField from "./ParticleField";
-import ScanlineEffect from "./ScanlineEffect";
 
 function GameAmbientEffects() {
   return (
     <div className="game-ambient-effects" aria-hidden="true">
       <AnimatedGridOverlay />
-      <AmbientGlow />
-      <ParticleField />
-      <FloatingSqlTokens />
-      <ScanlineEffect />
     </div>
   );
 }
